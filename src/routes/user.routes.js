952540a0e14504c9a2c86373ad19902d2322f96c
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2026-02-14 21:14:25 +0530

    home routes has been added here

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import { upload } from '../middleware/multer.middleware.js';
-import {  deleteBlog, editBlog, getAllBlogsWithCurentUser, getAllUsers, LikeBlog, login, logout, register, updateUserProfile, uploadBlog } from '../controller/user.controller.js';
+import { addBookmark, changePassword, deleteBlog, deleteUserAccount, editBlog, getAllBlogsWithCurentUser, getAllUsers, LikeBlog, login, logout, register, updateUserProfile, uploadBlog } from '../controller/user.controller.js';
 import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -27,7 +27,9 @@ router.route('/get-all-users').get(verifyJWT, getAllUsers)
 
 
 router.route('/update-user-profile').post(upload.fields([{ name: 'avatar', maxCount: 1 }]), verifyJWT, updateUserProfile)
-
+router.route("/bookmark-blog").post(verifyJWT , addBookmark)
+router.route("/change-password").post(verifyJWT , changePassword)
+router.route("/delete-account").post(verifyJWT , deleteUserAccount)
 
 
 
